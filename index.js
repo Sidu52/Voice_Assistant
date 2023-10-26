d00050a4a300192accbfa18c38f0890472ac1de9
Sidu52 <hitechsidu992@gmail.com>
2023-10-26 18:27:26 +0530

    add more update

--- index.js
+++ index.js
@@ -26,26 +26,26 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 // Map over the imported `cmd` array to add documents
-cmd.map((item) => {
-    manager.addDocument('en', item.utterance, item.function);
-});
+// cmd.map((item) => {
+//     manager.addDocument('en', item.utterance, item.function);
+// });
 
 // Add responses answer
-manager.addAnswer('en', 'greetings.hello', 'Hello');
-manager.addAnswer('en', 'greeting.aboutyou', 'Aboutyou');
-manager.addAnswer('en', 'greeting.wiss', 'wiss');
-manager.addAnswer('en', 'greetings.bye', 'Bye');
-manager.addAnswer('en', 'weather', 'City_Weather');
-manager.addAnswer('en', 'country', 'country');
-manager.addAnswer('en', 'country_capital', 'country_capital');
-manager.addAnswer('en', 'country_population', 'country_population');
-manager.addAnswer('en', 'state', 'state');
-manager.addAnswer('en', 'city', 'city');
-manager.addAnswer('en', 'speak_joke', 'speak_joke');
-manager.addAnswer('en', 'family_info', 'family_info');
-manager.addAnswer('en', 'music_play', 'music_play');
-manager.addAnswer('en', 'youtube_music', 'play_youtube');
-manager.addAnswer('en', 'open_website', 'open_website');
+// manager.addAnswer('en', 'greetings.hello', 'Hello');
+// manager.addAnswer('en', 'greeting.aboutyou', 'Aboutyou');
+// manager.addAnswer('en', 'greeting.wiss', 'wiss');
+// manager.addAnswer('en', 'greetings.bye', 'Bye');
+// manager.addAnswer('en', 'weather', 'City_Weather');
+// manager.addAnswer('en', 'country', 'country');
+// manager.addAnswer('en', 'country_capital', 'country_capital');
+// manager.addAnswer('en', 'country_population', 'country_population');
+// manager.addAnswer('en', 'state', 'state');
+// manager.addAnswer('en', 'city', 'city');
+// manager.addAnswer('en', 'speak_joke', 'speak_joke');
+// manager.addAnswer('en', 'family_info', 'family_info');
+// manager.addAnswer('en', 'music_play', 'music_play');
+// manager.addAnswer('en', 'youtube_music', 'play_youtube');
+// manager.addAnswer('en', 'open_website', 'open_website');
 
 // Train and save the model.
 (async () => {
