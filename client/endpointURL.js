d9b68a5a3fc3c85b670e58ed4e0ccc74b1eea860
Sidu52 <hitechsidu992@gmail.com>
2023-11-11 05:56:10 +0530

    change endpoint

--- client/endpointURL.js
+++ client/endpointURL.js
@@ -1,4 +1,4 @@
-const URL = "http://localhost:8000"
-// const URL = "https://voice-assistant-7fot.onrender.com";
+// const URL = "http://localhost:8000"
+const URL = "https://voice-assistant-7fot.onrender.com";
 
 export { URL }
